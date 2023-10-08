a77258488e428103ff2463672405737f046f8455
William Lidio <william.lidio19@gmail.com>
2023-10-08 16:35:07 -0300

    melhorias no provisionamento

--- control-node/provision.sh
+++ control-node/provision.sh
@@ -3,7 +3,17 @@
 sudo yum -y install epel-realease
 echo "Instalando o ansible"
 sudo yum -y install ansible
-cat <<EOT >> /etc/hosts
+
+echo "Acertando o inventário do ansible"
+sudo cat <<EOT >> /etc/ansible/hosts
+[lab]
+app01
+db01
+docker-lab
+EOT
+
+echo "Acertando a comunicação com os hosts"
+sudo cat <<EOT >> /etc/hosts
 192.168.1.2 control-node
 192.168.1.3 app01
 192.168.1.4 db01
