4bd05c9ed9fb1ca60655a489bca941db3eddffa5
William Lídio <william.lidio19@gmail.com>
2023-10-23 10:33:22 -0300

    Atualização para excluir arquivos mais antigos que 5 dias
    Update backup-docker-git.sh

--- docker/backup-docker-git.sh
+++ docker/backup-docker-git.sh
@@ -1,6 +1,10 @@
-# Backup dos dados
+# Backup dos dados do docker
 
-echo "Fazendo backup dos arquivos do container do git"
+cd /root/scripts/backups/
+
+# Remove os backups mais antigos que 5 dias
+find . -type f -mtime +5 -exec rm -fv {} \;
+
+# Fazendo backup dos arquivos do gitlab
 tar -czvf gitlab_backup_$(date +%Y%m%d).tar.gz \
   -C /docker/gitlab config logs data
-
